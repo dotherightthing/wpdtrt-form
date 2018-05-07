6e2dcb805e6a5914258620bc6f0de78abfee35e5
Dan Smith <dev@dotherightthing.co.nz>
2018-05-07 18:51:50 +1200

    Fix path to autoloader when loaded as a test dependency (dotherightthing/wpdtrt-plugin#104)

--- wpdtrt-forms.php
+++ wpdtrt-forms.php
@@ -12,7 +12,18 @@
  * Domain Path:  /languages
  */
 
-require_once plugin_dir_path( __FILE__ ) . "vendor/autoload.php";
+/**
+ * Autoload namespaced package classes
+ * @see https://github.com/dotherightthing/wpdtrt-plugin/wiki/Options:-Adding-WordPress-plugin-dependencies
+ */
+if ( defined( 'WPDTRT_FORMS_TEST_DEPENDENCY' ) ) {
+  $projectRootPath = realpath(__DIR__ . '/../../..') . '/';
+}
+else {
+  $projectRootPath = '';
+}
+
+require_once $projectRootPath . "vendor/autoload.php";
 
 /**
  * Constants
@@ -100,7 +111,7 @@ if( ! defined( 'WPDTRT_FORMS_URL' ) ) {
 
   // base class
   // redundant, but includes the composer-generated autoload file if not already included
-  require_once(WPDTRT_FORMS_PATH . 'vendor/dotherightthing/wpdtrt-plugin/index.php');
+  require_once($projectRootPath . 'vendor/dotherightthing/wpdtrt-plugin/index.php');
 
   // classes without composer.json files are loaded via Bower
   //require_once(WPDTRT_FORMS_PATH . 'vendor/name/file.php');
